18444cdcaa0c81844a6b52e8e231d5c72b8b6ada
Aarishbangash <157419986+Aarishbangash@users.noreply.github.com>
2024-04-01 01:33:26 +0500

    Time: 35 ms (27.78%) | Memory: 25.1 MB (27.47%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& str)
  {
 
-        //1. Store the values in the map..
+       
         map<string,vector<string>>mp;
 
         for(int i=0;i<str.size();i++)
@@ -15,13 +15,12 @@ public:
         }
   
 
-    //2. storing of the ans.
-        //size of the ans will be the no. of the rows. in mp.
+   
         vector<vector<string>> ans(mp.size());
         int index =0;
         for(auto x:mp)
         {
-            //to store the anagrams or same items together.
+           
             auto temp = x.second;
 
             for(auto x:temp)
